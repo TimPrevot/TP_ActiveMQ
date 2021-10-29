16b215aee238035cb0d2e4b9291f42359a8a8fe8
deadbang3r <timothe.prevot@efrei.net>
2021-10-29 10:57:19 +0200

    Initial commit

---
+++ src/main/java/receiver/MyReceiver2.java
@@ -0,0 +1,57 @@
+package receiver;
+
+import javax.jms.*;
+
+import javax.jms.Queue;
+import javax.jms.QueueConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyReceiver2 {
+
+    public static void main(String[] args) {
+        try {
+
+            ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContextJMS.xml");
+            QueueConnectionFactory factory = (QueueConnectionFactory) applicationContext.getBean("connectionFactory");
+
+            Queue queue = (Queue) applicationContext.getBean("queue");
+
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+            // Create a connection. See https://docs.oracle.com/javaee/7/api/javax/jms/package-summary.html
+            Connection connection = connectionFactory.createConnection();
+            // Open a session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            // Create a destination
+            Destination destination = session.createTopic("DTopic");
+            // Create a receive
+            MessageConsumer consumer = session.createConsumer(destination);
+            // start the connection
+            connection.start();
+            // Receive the message
+            consumer.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message) {
+                    try {
+                        if (message instanceof TextMessage) {
+                            TextMessage textMessage = (TextMessage) message;
+                            String text = textMessage.getText();
+                            System.out.println("Received: " + text);
+                        } else {
+                            System.out.println("Received: " + message);
+                        }
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/sender/MySender2.java
@@ -0,0 +1,54 @@
+package sender;
+
+import javax.jms.*;
+import javax.jms.QueueConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.w3c.dom.Text;
+
+public class MySender2 {
+
+    public static void main(String[] args) {
+
+        try {
+
+            ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContextJMS.xml");
+            QueueConnectionFactory factory = (QueueConnectionFactory) applicationContext.getBean("connectionFactory");
+
+            Queue queue = (Queue) applicationContext.getBean("queue");
+
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+            // Create a connection. See https://docs.oracle.com/javaee/7/api/javax/jms/package-summary.html
+            Connection connection = connectionFactory.createConnection();
+            // Open a session without transaction and acknowledge automatic
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            // Create a destination
+            Destination destination = session.createQueue("DQueue");
+            // Create a sender
+            MessageProducer producer = session.createProducer(destination);
+            producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+            //producer.setPriority(7);
+            // Start the connection
+            connection.start();
+            // Create a message
+            String text = "This is from sender2 !";
+            TextMessage message = session.createTextMessage(text);
+            // Send the message
+            producer.send(message);
+            System.out.println("Sent: " + message);
+            // Close the session
+            session.close();
+            // Close the connection
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
